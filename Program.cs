295b0813f3ff12b362786f9f822b9ee300b17ba9
Michael Khanda <michaelkhanda006@gmail.com>
2023-06-27 19:23:56 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DOWHILELOOP2_ContinueandBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, total = 0;
+
+
+            do
+            {
+                Console.WriteLine("Enter a number:");
+                int score = int.Parse(Console.ReadLine());
+                if ((score > 100) || (score < 0))
+                continue;
+                total = total + score;
+                i++;
+
+            } while (i < 5);
+            Console.WriteLine(total);
+        }
+    }
+}
